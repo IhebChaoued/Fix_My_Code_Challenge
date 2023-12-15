2aa31c844293f323bcd54f795d95dce9dbbaec79
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-15 10:35:38 +0000

    Updated

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,13 +43,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		tmp = *head;
-		if (tmp->prev != NULL)
-			tmp->prev->next = tmp->next;
-		if (tmp->next != NULL)
-			tmp->next->prev = tmp->prev;
-		free(tmp);
-		*head = saved_head;
+		(*head)->prev->prev = (*head)->prev;
+		free(*head);
+
+		if ((*head) != NULL)
+		{
+			if ((*head)->next != NULL)
+				(*head)->next->prev = (*head)->prev;
+			*head = saved_head;
+		}
 	}
+
 	return (1);
 }
