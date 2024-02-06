ed0f1e9075229c5dd945f9548192ca9df26dcc14
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-06 16:42:46 +0000

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,7 +10,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
@@ -20,8 +20,8 @@ class Square():
 
 
 if __name__ == "__main__":
-
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
+
