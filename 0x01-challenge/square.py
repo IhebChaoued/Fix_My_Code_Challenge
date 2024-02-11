091eb45c49be7be7b9cc09385d59f01a12053dc4
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-11 15:05:41 +0000

    updated

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
-"""A module declaring and calling a class Square"""
+"""a module declaring and calling a class Square"""
 
 
 class Square():
     """
-    Class Square definition. Methods:
-    - __init__: Initializes a new Square object
-    - area_of_my_square: Returns the area of a square
-    - perimeter_of_my_square: Returns the perimeter of a square
-    - __str__: Defines and returns the string representation
-      of a square
+    class square definition. Methods:
+    - constructor
+    - area_of_my_square: returns the area of a square
+    - perimeter_of_my_square: returns perimeter of a square
+    - str: defines and returns the string respresentation
+    of a square
     """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Initializes a new Square object"""
+        """initiates a new Square object"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Calculates and returns the area of the square"""
+        """ Area of the square """
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """Calculates and returns the perimeter of a square"""
+    def permiter_of_my_square(self):
+        """ Perimeter of a square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """String representation of a square"""
+        """ string representation of a square """
         return "{}/{}".format(self.width, self.height)
-
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
-    print("Area:", s.area_of_my_square())
-    print("Perimeter:", s.perimeter_of_my_square())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
